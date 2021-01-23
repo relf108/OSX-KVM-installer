0ec0b03548a482ea79efa202ddff0c5e866cf128
Tristan Sutton <tristan.sutton@gmail.com>
2021-01-23 16:47:17 +1100

    wsl

--- lib/src/windows_setup.dart
+++ lib/src/windows_setup.dart
@@ -14,11 +14,12 @@ class WindowsSetup {
       fetch(
           url:
               'https://downloads.sourceforge.net/project/vcxsrv/vcxsrv/1.20.9.0/vcxsrv-64.1.20.9.0.installer.exe?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fvcxsrv%2Ffiles%2Flatest%2Fdownload&ts=1611373071',
-          saveToPath: '$HOME\\..\\..\\Downloads\\vcxsrv-64.1.20.9.0.installer.exe');
+          saveToPath:
+              '$HOME\\..\\..\\Downloads\\vcxsrv-64.1.20.9.0.installer.exe');
     }
 
-    '.\\vcxsrv-64.1.20.9.0.installer.exe'
-        .start(workingDirectory: '$HOME\\..\\..\\Downloads', privileged: true);
+    '.\\Downloads\\vcxsrv-64.1.20.9.0.installer.exe'
+        .start(workingDirectory: '$HOME\\..\\..', privileged: true);
     'wsl pub global activate osx_kvm_installer'.start(privileged: true);
   }
 }
