bbffceabe74d49f9ab66cad266096ee1ee88b63d
Tristan Sutton <tristan.sutton@gmail.com>
2021-01-23 16:42:22 +1100

    wsl

--- lib/src/windows_setup.dart
+++ lib/src/windows_setup.dart
@@ -12,14 +12,14 @@ class WindowsSetup {
   static void wslX11Setup() {
     print('$HOME');
 
-    if (!exists('$HOME\\..\\..\\OSX-KVM-installer\\vcxsrv-64.1.20.8.1.installer.exe')) {
+    if (!exists('$HOME\\..\\..\\OSX-KVM-installer\\vcxsrv-64.1.20.9.0.installer.exe')) {
       fetch(
           url:
               'https://downloads.sourceforge.net/project/vcxsrv/vcxsrv/1.20.9.0/vcxsrv-64.1.20.9.0.installer.exe?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fvcxsrv%2Ffiles%2Flatest%2Fdownload&ts=1611373071',
-          saveToPath: '$HOME\\..\\..\\Downloads\\vcxsrv-64.1.20.8.1.installer.exe');
+          saveToPath: '$HOME\\..\\..\\Downloads\\vcxsrv-64.1.20.9.0.installer.exe');
     }
 
-    './vcxsrv-64.1.20.8.1.installer.exe'
+    '.\\vcxsrv-64.1.20.9.0.installer.exe'
         .start(workingDirectory: '$HOME\\..\\..\\Downloads', privileged: true);
     'wsl pub global activate osx_kvm_installer'.start(privileged: true);
   }
